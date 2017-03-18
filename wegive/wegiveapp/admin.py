517ec261078c38ddaffe804df58683b0fffa1403
Aashirya <ak1107@wildcats.unh.edu>
2017-03-17 21:20:22 -0500

    Added stuff

--- wegive/wegiveapp/admin.py
+++ wegive/wegiveapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Charity, Donor
 
-# Register your models here.
+
+admin.site.register(Charity)# Register your models here.
+admin.site.register(Donor)
--- wegive/wegiveapp/models.py
+++ wegive/wegiveapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Charity(models.Model):
+	name = models.CharField(max_length=2000,null=False)
+	Address = models.TextField(null=False)
+	Phone = models.TextField(null=False)
+	cause = models.TextField(null=False)
+
+	def __str__(self):
+		return self.name
+
+
+class Donor(models.Model):
+	name = 	models.CharField(max_length=2000,null=False)
+	Address = models.TextField(null=False)
+	Phone = models.TextField(null=False)
+	DOB = models.TextField(null=False)
+
+
+	def __str__(self):
+		return self.name
