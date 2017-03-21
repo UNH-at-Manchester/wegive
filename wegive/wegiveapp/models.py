b0c26df85e4a12a2a1b0159a2a7819c5f2d05647
John Smith <nbwcoc@openmailbox.org>
2017-03-21 16:39:42 -0400

    implement initial search functionality. Also some miscellaneous changes

--- wegive/wegiveapp/models.py
+++ wegive/wegiveapp/models.py
@@ -2,20 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Charity(models.Model):
-    name = models.CharField(max_length=2000,null=False)
-    Address = models.TextField(null=False)
-    Phone = models.TextField(null=False)
-    cause = models.TextField(null=False)
+    name = models.CharField(max_length=2000, null=False, blank=False)
+    address = models.TextField(null=False, default="")
+    location_x = models.FloatField(null=False, default=1000.0);
+    location_y = models.FloatField(null=False, default=1000.0)
+    phone = models.TextField(null=False, default="")
+    cause = models.TextField(null=False, default="")
 
     def __str__(self):
         return self.name
 
 
 class Donor(models.Model):
-    name =  models.CharField(max_length=2000,null=False)
-    Address = models.TextField(null=False)
-    Phone = models.TextField(null=False)
-    DOB = models.TextField(null=False)
+    name =  models.CharField(max_length=2000, null=False, blank=False)
+    address = models.TextField(null=False)
+    phone = models.TextField(null=False)
+    date_of_birth = models.TextField(null=False)
 
 
     def __str__(self):
--- wegive/wegiveapp/views.py
+++ wegive/wegiveapp/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from . import models
+from . import forms
 
 # Create your views here.
+
+def match_charity(name="", tags="", near_x=0.0, near_y=0.0, near_distance=0.0):
+    """
+    Usage: match_charity(name="", tags="", near_x=0.0, near_y=0.0)
+    Searches for charities with the given search terms.
+
+    Parameters:
+    string name: name of the charity
+    string tags: csv of tags. Not implemented
+    float near_x: origin x coordinate for a location based search
+    float near_y: origin y coordinate for a location based search
+    float near_distance: radius for a location based search.
+
+    Returns:
+    The resulting query set.
+    """
+    charities = models.Charity.objects()
+    # construct kwargs
+    kwargs = {}
+    if name != "":
+        kwargs["name__contains"] = name
+    if tags != "":                               # NOT IMPLEMENTED
+        pass
+    if near_x != 0 and near_y != 0 and near_distance != 0:
+        min_y = near_y - near_distance
+        max_y = near_y + near_distance
+        min_x = near_x - near_distance
+        max_x = near_x + near_distance
+
+        kwargs["location_x__gte"] = min_x
+        kwargs["location_x__lte"] = max_x
+        kwargs["location_y__gte"] = min_y
+        kwargs["location_y__lte"] = max_y
+
+    # get query set
+    results_qset = charities.get(**kwargs)
+
+    return results_qset
+
+def search(request):
+    """
+    Search for charities.
+    When accessed through GET, gives a search form.
+    When accessed through POST, gives a table with search results. Each result
+    will link to a select request.
+    """
+    if request.method == "POST":
+        # TODO: near me test
+        res = match_charity(
+            name=request.POST[name], near_x=request.POST[location_x],
+            near_y=request.POST[location_y], near_distance=request.POST[radius])
+        return render(request, "templates/html/search-results.html", {"res": res})
+    else:
+        form = forms.SearchForm()
+        return render(request, "templates/html/search.html", {"form": form})
+
+def select(request):
+    pass
+
+def pay(request):
+    pass
