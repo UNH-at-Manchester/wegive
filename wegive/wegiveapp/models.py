6a71d82b0005ad964cdb6b7efdb71dde1ecb95cf
John Smith <nbwcoc@openmailbox.org>
2017-03-21 13:56:41 -0400

    minor stylistic changes

--- wegive/wegiveapp/models.py
+++ wegive/wegiveapp/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Charity(models.Model):
-	name = models.CharField(max_length=2000,null=False)
-	Address = models.TextField(null=False)
-	Phone = models.TextField(null=False)
-	cause = models.TextField(null=False)
+    name = models.CharField(max_length=2000,null=False)
+    Address = models.TextField(null=False)
+    Phone = models.TextField(null=False)
+    cause = models.TextField(null=False)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 class Donor(models.Model):
-	name = 	models.CharField(max_length=2000,null=False)
-	Address = models.TextField(null=False)
-	Phone = models.TextField(null=False)
-	DOB = models.TextField(null=False)
+    name =  models.CharField(max_length=2000,null=False)
+    Address = models.TextField(null=False)
+    Phone = models.TextField(null=False)
+    DOB = models.TextField(null=False)
 
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
