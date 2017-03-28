18a7cab4eca3771f29b5faf9b71573e8873129d4
John Smith <nbwcoc@openmailbox.org>
2017-03-28 18:31:15 -0400

    Rewrite match_charity() and search() in wegiveapp/views.py to fully implement tags.

--- wegive/wegiveapp/forms.py
+++ wegive/wegiveapp/forms.py
@@ -1,11 +1,15 @@
 from django import forms
+from . import tags
 
 class SearchForm(forms.Form):
-    name = forms.CharField(label="Charity name", max_length=2000)
+    name = forms.CharField(label="Charity name", max_length=2000, required=False)
     # TODO: tags
     # IDEA: get the x and y from the user's address, check OSM Nominatim
     near_me = forms.BooleanField(label="Near Me", required=False)
     location_x = forms.FloatField(label="Location (x)", required=False)
     location_y = forms.FloatField(label="Location(y)", required=False)
     radius = forms.FloatField(label="Radius", required=False)
+    tags = forms.MultipleChoiceField(label="Tags", required=False,
+                                     widget=forms.CheckboxSelectMultiple,
+                                     choices=[(i, i) for i in tags.tags])
 
--- wegive/wegiveapp/views.py
+++ wegive/wegiveapp/views.py
@@ -4,42 +4,43 @@ from . import forms
 
 # Create your views here.
 
-def match_charity(name="", tags="", near_x=0.0, near_y=0.0, near_distance=0.0):
+def match_charity(name="", tags=[], location_x=0.0, location_y=0.0, radius=0.0):
     """
-    Usage: match_charity(name="", tags="", near_x=0.0, near_y=0.0)
+    Usage: match_charity(name="", tags="", location_x=0.0, location_y=0.0)
     Searches for charities with the given search terms.
+
+    Note that for a location based search location_x, location_y, and radius
+    must ALL be set.
 
     Parameters:
     string name: name of the charity
-    string tags: csv of tags. Not implemented
-    float near_x: origin x coordinate for a location based search
-    float near_y: origin y coordinate for a location based search
-    float near_distance: radius for a location based search.
+    string[] tags: List of strings (as would be returned from a form) of tags
+                   to match.
+    float location_x: origin x coordinate for a location based search
+    float location_y: origin y coordinate for a location based search
+    float radius: radius for a location based search.
 
     Returns:
     The resulting query set.
     """
+    results_qset = models.Charity.objects.filter()
     # construct kwargs
     kwargs = {}
     if name != "":
-        kwargs["name__contains"] = name
-    if tags != "":                               # NOT IMPLEMENTED
-        pass
+        results_qset = results_qset.filter(name__contains=name)
+    if tags != "":
+        for tag in tags:
+            results_qset = results_qset.filter(tags_csv__contains=tag)
     # parentheses here for implicit line completion
-    if (near_x != 0 and near_y != 0 and near_distance != 0 and
-        near_x != "" and near_y != "" and near_distance != ""):
-        max_y = float(near_y) + float(near_distance)
-        min_x = float(near_x) - float(near_distance)
-        min_y = float(near_y) - float(near_distance)
-        max_x = float(near_x) + float(near_distance)
+    if (location_x != 0 and location_y != 0 and radius != 0 and
+        location_x != "" and location_y != "" and radius != ""):
+        max_y = float(location_y) + float(radius)
+        min_x = float(location_x) - float(radius)
+        min_y = float(location_y) - float(radius)
+        max_x = float(location_x) + float(radius)
 
-        kwargs["location_x__gte"] = min_x
-        kwargs["location_x__lte"] = max_x
-        kwargs["location_y__gte"] = min_y
-        kwargs["location_y__lte"] = max_y
-
-    # get query set
-    results_qset = models.Charity.objects.filter(**kwargs)
+        results_qset = results_qset.filter(location_x__gte=min_x, location_x_lte=max_x,
+                            location_y__gte=min_y, location_y__lte=max_y)
 
     return results_qset
 
@@ -52,9 +53,12 @@ def search(request):
     """
     if request.method == "POST":
         # TODO: near me test
-        res = match_charity(
-            name=request.POST["name"], near_x=request.POST["location_x"],
-            near_y=request.POST["location_y"], near_distance=request.POST["radius"])
+        kwargs = {}
+        for item in request.POST:
+            if item in ("name", "location_x", "location_y", "radius", "tags"):
+                kwargs[item] = request.POST[item]
+
+        res = match_charity(**kwargs)
         return render(request, "html/search-results.html", {"res": res})
     else:
         form = forms.SearchForm()
