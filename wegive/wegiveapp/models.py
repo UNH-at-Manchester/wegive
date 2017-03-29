30a5321a3578ca2ce68a64794a73afc6c4c00352
John Smith <nbwcoc@openmailbox.org>
2017-03-29 16:11:46 -0400

    Model for donations. Also, associate charities and donors with users.

--- wegive/wegiveapp/models.py
+++ wegive/wegiveapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Charity(models.Model):
@@ -8,10 +9,12 @@ class Charity(models.Model):
     location_y = models.FloatField(null=False, default=1000.0)
     phone = models.TextField(null=False, default="")
     cause = models.TextField(null=False, default="")
-    tags_csv = models.TextField(null=False, default="")
+    tags_csv = models.TextField(null=False, default="", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False,
+                                blank=False)
 
     def __str__(self):
-        return self.name
+        return "{} ({})".format(self.user, self.name)
 
 
 class Donor(models.Model):
@@ -19,8 +22,27 @@ class Donor(models.Model):
     address = models.TextField(null=False)
     phone = models.TextField(null=False)
     date_of_birth = models.TextField(null=False)
-    tags_csv = models.TextField(null=False, default="")
+    tags_csv = models.TextField(null=False, default="", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False,
+                                blank=False)
 
 
     def __str__(self):
-        return self.name
+        return "{} ({})".format(self.user, self.name)
+
+class Donation(models.Model):
+    """
+    Model for donation records. All currencies USD.
+
+    Fields:
+    Donor donor: The donor who made the donation.
+    Charity charity: The charity the donation was made to.
+    Decimal amount: The dollar amount. This is a Python decimal object, not a
+                    float, so we don't have to worry about precision.
+    DateTime time: The date and time at which the donation took place.
+    """
+    donor = models.OneToOneField(Donor, on_delete=models.CASCADE)
+    charity = models.OneToOneField(Charity, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=16, decimal_places=3, null=False,
+                                 blank=False)
+    time = models.DateTimeField(auto_now_add=True, null=False, blank=False)
