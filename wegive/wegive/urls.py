ba6f1356e78ac0456c9ee43c09ccbbcf74d36544
tinkhamgreg <gjtinkha@wildcats.unh.edu>
2017-04-25 13:10:39 -0400

    added navbar, search with map is functional. Need to add navbar to all pages, footer as well. Paypal button also added.

--- wegive/wegive/urls.py
+++ wegive/wegive/urls.py
@@ -21,10 +21,10 @@ from wegiveapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"^search/", views.search),
+    url(r"^search/", views.search, name='search'),
     url(r"^loginat/", views.loginat),
     url(r"^logoutat/", views.logoutat),
-    url(r"^sign_up/", views.sign_up),
+    url(r"^sign_up/", views.sign_up, name='signup'),
     url(r"^record/", views.view_records),
     url(r"^api/", views.api),
     url(r"^select/(?P<id>\d+)/$", views.select),
