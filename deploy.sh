b3e16f02fa549168e8524a21c2b705101eac2a60
John Smith <nbwcoc@openmailbox.org>
2017-05-03 18:32:08 -0400

    Add robots.txt and favicon.ico and update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,33 +7,35 @@
 # this script should be in /wegive/deploy.
 
 
-# DEFINE THIS
+# DO NOT DEFINE THIS IN THE REPO
 SECRET_KEY=secret
+mkdir /tmp/wegive.deploy
+cd /tmp/wegive.deploy
 git clone "https://github.com/UNH-at-Manchester/wegive.git"
 cd wegive/wegive
 rm -rf .git
 rm db.sqlite3
+if [ -e /usr/local/www/wegive/db.sqlite3 ] ; then
+    cp /usr/local/www/wegive/db.sqlite3 .
+else
+    echo "no database!";
+fi
 sed "s/DEBUG = True/DEBUG = False/g" wegive/settings.py > wegive/settings.py.new
 mv wegive/settings.py.new wegive/settings.py
 sed "s/SECRET_KEY = .*/SECRET_KEY = '$SECRET_KEY'/g" wegive/settings.py > wegive/settings.py.new
 mv wegive/settings.py.new wegive/settings.py
 cd ..
 
-if [ -e ../../wegive ] ; then
-    cd ../../wegive
-    killall django
-    cd ..
-    mv wegive/wegive/db.sqlite3 .
-    rm -rf wegive;
-    mkdir wegive
-    mkdir wegive/wegive
-else
-    cd ../../;
+if [ -e /usr/local/www/wegive ] ; then
+    rm -rf /usr/local/www/wegive;
+    mkdir /usr/local/www/wegive
 fi
 
-mv deploy/wegive wegive
+mv wegive /usr/local/www/
 
 if [ -e db.sqlite3 ] ; then
-    mv db.sqlite3 wegive/wegive/db.sqlite3
+    mv db.sqlite3 /usr/local/www/wegive/db.sqlite3
 fi
+cd
+rm -rf /tmp/wegive.deploy
 # should be done
